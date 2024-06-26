f86a2252beed2dad1de615fbb8c12dcb04acb741
AnkitChaudhary0434 <ankitchaudhary0434@gmail.com>
2024-06-26 23:10:19 +0530

    Add JavaScript datatypes file

---
+++ javascript/01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as strict mode
+
+// alert( 3+3 ) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3); // code readabilaity should be high. like this console.log(3+3); 
+
+console.log("jaat");
+
+let name = "bobby";
+let age = 17;
+let isloggedin = true;
+let state;
+
+
+// number => 2 to power 53
+// bigint 
+// string => "", ''  
+// boolean => true,false
+// null => standalone value
+// undefined => 
+//symbol => unique value
+
+
+// object => {key:value}
+
+console.log(typeof "bobby"); // string
+console.log(typeof age); // number
+console.log(typeof isloggedin); // boolean
+console.log(typeof state); // undefined
+console.log(typeof null); // object
