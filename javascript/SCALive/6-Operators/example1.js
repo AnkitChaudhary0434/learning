a8ea07c410a798f476eccaf95009cc0cd8d5e1a2
AnkitChaudhary0434 <ankitchaudhary0434@gmail.com>
2024-07-01 02:56:56 +0530

    feat: Add dialog box examples and operators in JavaScript

---
+++ javascript/SCALive/6-Operators/example1.js
@@ -0,0 +1,75 @@
+// Arithmetic Operators
+
+// let a = 10;
+// b = 4;
+// console.log("a=", a, "b=", b, "a+b=", a + b);
+// console.log("a=", a, "b=", b, "a-b=", a - b);
+// console.log("a=", a, "b=", b, "a*b=", a * b);
+// console.log("a=", a, "b=", b, "a/b=", a / b);
+// console.log("a=", a, "b=", b, "a%b=", a % b);
+// console.log("a=", a, "b=", b, "a**b=", a ** b);
+
+// unary increment operator
+
+// a = 10;
+// console.log(a);
+// a++; //post increment
+// console.log(a);
+
+// a = 10;
+// console.log(a);
+// ++a; //pre increment
+// console.log(a);
+
+// a = 10;
+// let c;
+// c = a++;
+// console.log("a=", a, "c=", c);
+
+// a = 10;
+// c = ++a;
+// console.log("a=", a, "c=", c);
+
+// a = 10;
+// c = a--;
+// console.log("a=", a, "c=", c);
+
+// a = 10;
+// c = --a;
+// console.log("a=", a, "c=", c);
+
+// comparison operators
+
+// a = 10;
+// b = 10;
+// console.log("a=", a, "b=", b, "a>b", a > b);
+// console.log("a=", a, "b=", b, "a<b", a < b);
+// console.log("a=", a, "b=", b, "a>=b", a >= b);
+// console.log("a=", a, "b=", b, "a<=b", a <= b);
+// console.log(a==b)
+// c="10"
+// console.log(a==c);
+// console.log(a===c);
+// console.log(a!=c);
+// console.log(a!==c);
+
+// Logical Operators
+
+// a = 10;
+// b = 10;
+// c = 10;
+// console.log(a == b && b == c);
+// console.log(a == b || b == c);
+// console.log(!(a == b));
+
+//arathmatic assignment operators
+
+// let x=10;
+// let y = 5;
+
+// console.log(x=y);
+// console.log(x+=y);
+// console.log(x-=y);
+// console.log(x*=y);
+// console.log(x/=y);
+// console.log(x%=y);
