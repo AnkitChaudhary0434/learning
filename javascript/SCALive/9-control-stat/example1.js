183e926fdbdf5aec4acbc48523a5ab312a5161ba
AnkitChaudhary0434 <ankitchaudhary0434@gmail.com>
2024-07-03 02:21:05 +0530

    if, if else, if else if else

---
+++ javascript/SCALive/9-control-stat/example1.js
@@ -0,0 +1,40 @@
+//control stat-part 1
+
+//if statement
+
+// let n=Number(prompt("Enter a number:"));
+// if(n>0){
+//     document.write("the number is positive")
+// }
+// if(n<0){
+//     document.write("the number is negative")
+// }
+// if(n==0){
+//     document.write("the number is zero")
+// }
+// document.write("</br><hr>end of program")
+
+//if else statement
+
+// let a=Number(prompt("Enter a number:"));
+// if(a>0){
+//     document.write("the number is positive")
+// }
+// else{
+//     document.write("the number is negative")
+// }
+// document.write("<br><hr>end of program")
+
+//if else if else statement
+
+let n=Number(prompt("Enter a number:"));
+if(n>0){
+    document.write("the number is positive")
+}
+else if(n<0){
+    document.write("the number is negative")
+}
+else{
+    document.write("the number is zero")
+}
+document.write("</br><hr>end of program")
