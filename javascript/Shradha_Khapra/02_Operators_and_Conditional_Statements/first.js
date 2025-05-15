828fb9c05f98a1d517800c2df3b62c66ae9c13b3
AnkitChaudhary0434 <ankitchaudhary0434@gmail.com>
2025-05-15 20:18:06 +0530

    Operators and Conditional Statements

---
+++ javascript/Shradha_Khapra/02_Operators_and_Conditional_Statements/first.js
@@ -0,0 +1,48 @@
+// comment
+
+
+//single line comment
+// this is printing hello
+console.log("hello");
+//multiline comment
+/*this is printing the name
+of student of gla u*/
+console.log("ankit chaudhary");
+
+
+//  Operators
+
+
+// arithmetic  Operators
+// let a = 2;
+// let b = 3;
+
+// console.log(`a = ${a} & b = ${b}`);
+// console.log(`a - b = ${a - b}`);
+// console.log(`a + b = ${a + b}`);
+// console.log(`a * b = ${a * b}`);
+// console.log(`a / b = ${a / b}`);
+
+// modulus
+// console.log(`a % b = ${a % b}`);
+
+// exponentiation
+// console.log(`a ** b = ${a ** b}`);
+
+// unary
+// let a = 2;
+// let b = 3;
+// console.log(`a = ${a} & b = ${b}`);
+// a = a + 1; // same as a++
+// a++;
+// a = a- 1 // same as a--
+// b--;
+// console.log(`a = ${a}`);
+// console.log(`b = ${b}`);
+
+// post / pre increment-decriment
+let a = 3;
+let b = 3;
+console.log(`a = ${a} & b = ${b}`);
+console.log(`++a = ${++a}`);
+console.log(`b-- = ${b--}`);
