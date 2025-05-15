25b1755b34aea3106698991e295294230157c612
AnkitChaudhary0434 <ankitchaudhary0434@gmail.com>
2025-05-15 20:16:21 +0530

    Variables & Data Types

---
+++ javascript/Shradha_Khapra/01_Variables_&_Data_Types/first.js
@@ -0,0 +1,85 @@
+// fullName = "bobby chaudhary jaat";
+// console.log(fullName);
+// age = 25;
+// price = 22.22;
+// x = null;
+// y = undefined;
+// isFollow = true;
+// console.log(isFollow);
+// fullName = 24; // dinamically typed
+// console.log(fullName);
+
+// case sensitive
+// fullname = "ankit chaudhary";
+// FULLNAME = "bobby chaudhary";
+// console.log(fullname);
+// console.log(FULLNAME);
+
+// no spase or some basics rools
+// full name = "bobby"; // not allowed
+
+// using keywords for creating variavle
+
+// var age = 23; // allow to re declar.
+// var age = 24; // allow to re declar.
+// var age = 25; // allow to re declar.
+
+// let age = 23; // dont allow to re declar.
+// let age = 24; // dont allow to re declar.
+// let age = 25; // dont allow to re declar.
+// but can update
+// age = 34;
+// age = 35;
+
+// // // const
+// const age = 34;
+// // // cannot be re declared aswellas not we updated
+// // const age  = 35; // can't
+// age = 36; // can't
+
+// empty variable
+// let a;
+// console.log(a); //output : undefined
+// but not allowed in const
+// const age; // output error
+
+
+// data types
+
+
+//primitive data type
+// let a = 23;
+// console.log(typeof a);
+// let fullName = "ankit chaudhary";
+// console.log(typeof fullName);
+// isFollow = true;
+// console.log(typeof isFollow);
+// let x;
+// console.log(typeof x);
+// let y = null;
+// console.log(typeof y);
+// let bi = BigInt("1234");
+// console.log(typeof bi);
+// let s = Symbol("hello !!");
+// console.log(typeof s);
+
+// non primitive
+// const student = {
+//   fullName : "ankit chaudhary",
+//   age : 23,
+//   cgpi : 8.2,
+//   isPass : true
+// };
+// console.log(student);
+// console.log(typeof student);
+// console.log(student["fullName"]);
+// console.log(student.fullName);
+// student.fullName = "rahul singh";
+// console.log(student.fullName);
+// console.log(student.age);
+// student.age = student.age + 1;
+// console.log(student.age);
+
+// let update
+// const not update
+// const object update keys
---
+++ javascript/Shradha_Khapra/01_Variables_&_Data_Types/practice.js
@@ -0,0 +1,21 @@
+// create a const object called "product" to store information about pan.
+const product = {
+  pName : "Parker jotter",
+  pRating : 7,
+  pOffer : 5,
+  pPrice : 270
+};
+console.log(product);
+console.log(typeof product);
+
+// CREATE A CONST OBJECT CALLED "profil" TO STORE INFORMATION OF ANY PROFILE
+const profile = {
+  pName : "@ankitttt_chaudhary",
+  pFollowers : 555,
+  pFollowing : 1,
+  isFollow : true,
+  bio : "6ft, desi JAAT, mother ladla, follows dreams"
+};
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile.pName);
